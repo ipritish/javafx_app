aa1e4c6f84c39094eada9e81d71d3d87b76f783c
ipritish <pritishjain2009@gmail.com>
2016-02-18 13:05:29 +0530

    javafx form handle

--- learnjavafx/src/main/java/com/learnjavafx/learnjavafx/App.java
+++ learnjavafx/src/main/java/com/learnjavafx/learnjavafx/App.java
@@ -4,10 +4,70 @@ package com.learnjavafx.learnjavafx;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.scene.paint.*;
+ 
+@SuppressWarnings("restriction")
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) 
     {
-        System.out.println( "Hello World!" );
+        primaryStage.setTitle("JavaFx Welcome");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+        
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+        
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        	 
+            public void handle(ActionEvent e) {
+				actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+            }
+
+        });
+        
+        primaryStage.setScene(new Scene(grid, 300, 275));
+        primaryStage.show();
     }
 }
